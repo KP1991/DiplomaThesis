08df8e0fb6e4d001601933bdde435cf6fe6a3997
KP1991 <p.kerbl@hotmail.com>
2015-10-20 19:13:35 +0200

    newapp2

--- TimeRecordingSystem_CarControl/ConsoleApp/Program.cs
+++ TimeRecordingSystem_CarControl/ConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("newTestApp");
+            Console.WriteLine("newTestApp2");
         }
     }
 }
